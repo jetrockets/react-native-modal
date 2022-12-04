99359d4612b2f7f442d5f3b6273ef454184bf199
Maksim Romanov <maksim.romanov@jetrockets.pro>
2022-12-04 22:09:28 +0400

    Modal updates

--- src/GestureModal/GestureModal.tsx
+++ src/GestureModal/GestureModal.tsx
@@ -5,12 +5,12 @@ import {
   LayoutRectangle,
   Modal as RNModal,
   Platform,
-  Pressable,
   StyleSheet,
+  TouchableOpacity,
   useWindowDimensions,
   View,
 } from 'react-native';
-import { gestureHandlerRootHOC, PanGestureHandler } from 'react-native-gesture-handler';
+import { gestureHandlerRootHOC, GestureHandlerRootView, PanGestureHandler } from 'react-native-gesture-handler';
 import Animated, {
   interpolateColor,
   runOnJS,
@@ -26,7 +26,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Props } from './types';
 
 const STARTING_POSITION = 100;
-const SH_COLORS = [rgba('#000', 0.5), rgba('#000', 0)];
+const SH_COLORS = [rgba('#000', 0.4), rgba('#000', 0)];
 
 const styles = StyleSheet.create({
   overlay: { zIndex: -1, position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 },
@@ -34,22 +34,31 @@ const styles = StyleSheet.create({
 
 const _GestureModal = React.memo(({ visible, setVisible, children, destroy }: Props) => {
   const windowDimensions = useWindowDimensions();
-  const contentHeightRef = React.useRef<LayoutRectangle['height']>(windowDimensions.height);
+  // const contentHeightRef = React.useRef<LayoutRectangle['height']>(windowDimensions.height);
+  const [contentHeight, setContentHeight] = React.useState<LayoutRectangle['height']>(windowDimensions.height);
 
   const scrollY = useSharedValue(windowDimensions.height);
+
+  const targetHeight = React.useMemo(() => contentHeight + STARTING_POSITION, [contentHeight]);
 
   useAnimatedReaction(
     () => {
       if (visible) return STARTING_POSITION;
 
-      return contentHeightRef.current;
+      return targetHeight;
     },
     (result, previous) => {
       if (result === previous) return;
 
-      scrollY.value = withTiming(result, {}, () => {
-        if (result === contentHeightRef.current) runOnJS(destroy)();
-      });
+      if (result === STARTING_POSITION && Platform.OS === 'ios') {
+        return (scrollY.value = withSpring(result, { damping: 14, restSpeedThreshold: 250 }));
+      }
+
+      if (result === targetHeight) {
+        return (scrollY.value = withTiming(result, {}, () => runOnJS(destroy)()));
+      }
+
+      scrollY.value = withTiming(result, {});
     },
     [visible],
   );
@@ -61,8 +70,8 @@ const _GestureModal = React.memo(({ visible, setVisible, children, destroy }: Pr
       scrollY.value = Math.round(STARTING_POSITION + event.translationY);
     },
     onEnd: (event) => {
-      console.log('contentHeightRef.current', event.translationY, windowDimensions.height, contentHeightRef.current);
-      if (event.translationY >= contentHeightRef.current / 2) return runOnJS(setVisible)(false);
+      const pointYToClose = (windowDimensions.height / contentHeight) * 100;
+      if (event.translationY >= pointYToClose) return runOnJS(setVisible)(false);
 
       if (Platform.OS === 'ios') return (scrollY.value = withSpring(STARTING_POSITION));
       scrollY.value = withTiming(STARTING_POSITION, {});
@@ -70,7 +79,7 @@ const _GestureModal = React.memo(({ visible, setVisible, children, destroy }: Pr
   });
 
   const animatedStyles = useAnimatedStyle(() => {
-    return { backgroundColor: interpolateColor(scrollY.value, [0, contentHeightRef.current], SH_COLORS) };
+    return { backgroundColor: interpolateColor(scrollY.value, [0, contentHeight], SH_COLORS) };
   });
 
   const containerStyles = useAnimatedStyle(() => ({ transform: [{ translateY: scrollY.value }] }));
@@ -79,14 +88,14 @@ const _GestureModal = React.memo(({ visible, setVisible, children, destroy }: Pr
     <Animated.View style={[StyleSheet.flatten({ flex: 1 }), animatedStyles]}>
       <PanGestureHandler onGestureEvent={eventHandler}>
         <Animated.View style={[StyleSheet.flatten({ flex: 1, justifyContent: 'flex-end' }), containerStyles]}>
-          <View onLayout={({ nativeEvent }) => (contentHeightRef.current = nativeEvent.layout.height)}>
+          <View onLayout={({ nativeEvent }) => setContentHeight(nativeEvent.layout.height - STARTING_POSITION)}>
             {children}
 
             <View style={StyleSheet.flatten({ height: STARTING_POSITION, backgroundColor: '#fff' })} />
           </View>
 
           {/* Overlay start */}
-          <Pressable
+          <TouchableOpacity
             onPress={() => setVisible(false)}
             style={[styles.overlay, StyleSheet.flatten({ top: -STARTING_POSITION })]}
           />
@@ -107,11 +116,13 @@ const GestureModal = (props: Omit<Props, 'destroy'>) => {
   }, [props.visible]);
 
   return (
-    <RNModal transparent={true} visible={visible}>
-      <SafeAreaProvider>
-        <GesturedModal {...props} destroy={() => setVisible(false)} />
-      </SafeAreaProvider>
-    </RNModal>
+    <GestureHandlerRootView style={StyleSheet.flatten({ flex: 1 })}>
+      <RNModal transparent={true} visible={visible}>
+        <SafeAreaProvider>
+          <GesturedModal {...props} destroy={() => setVisible(false)} />
+        </SafeAreaProvider>
+      </RNModal>
+    </GestureHandlerRootView>
   );
 };
 
