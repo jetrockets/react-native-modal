8a9eed055753a364c8480515f471ca739e897af8
Maksim Romanov <maksim.romanov@jetrockets.pro>
2022-12-06 01:04:24 +0400

    Examples work

--- examples/android/app/src/main/java/com/examples/MainActivity.java
+++ examples/android/app/src/main/java/com/examples/MainActivity.java
@@ -1,8 +1,10 @@
 package com.examples;
 
 import com.facebook.react.ReactActivity;
+
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +15,16 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "examples";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 
   /**
