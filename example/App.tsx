20887b84a9994578ff691810accdada4be6aba7a
Maksim Romanov <maksim.romanov@jetrockets.pro>
2022-12-06 11:14:34 +0400

    Example finish

--- example/App.tsx
+++ example/App.tsx
@@ -8,116 +8,64 @@
  * @format
  */
 
-import React, {type PropsWithChildren} from 'react';
+import React from 'react';
 
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}
-      >
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}
-      >
-        {children}
-      </Text>
-    </View>
-  );
-};
+import { GestureModal } from '@jetrockets/react-native-modal';
+import { Button, Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [visible, setVisisble] = React.useState(false);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const openModal = React.useCallback(() => setVisisble(true), []);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}
-      >
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}
-        >
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={StyleSheet.flatten({ flex: 1, justifyContent: 'center', alignItems: 'center' })}>
+      <Button title="Open modal" onPress={openModal} />
+      <GestureModal visible={visible} setVisible={setVisisble}>
+        <View style={styles.wrapper}>
+          <View style={styles.pullTargetContainer}>
+            <View style={styles.pullTarget} />
+          </View>
+
+          <View style={styles.container}>
+            <Text style={styles.previewText}>Hello world</Text>
+          </View>
         </View>
-      </ScrollView>
+      </GestureModal>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  previewText: {
+    fontSize: 32,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  wrapper: {
+    minHeight: 300,
+    backgroundColor: '#fff',
+    borderTopRightRadius: 32,
+    borderTopLeftRadius: 32,
+    paddingHorizontal: 12,
+    paddingTop: 12,
+    paddingBottom: Platform.select({ ios: 0, android: 24 }),
   },
-  highlight: {
-    fontWeight: '700',
+  pullTargetContainer: {
+    position: 'absolute',
+    top: 3,
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+  },
+  pullTarget: {
+    width: 50,
+    height: 4,
+    borderRadius: 8,
+    backgroundColor: '#f0f0f0',
   },
 });
 
